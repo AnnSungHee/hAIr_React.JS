c089f5918b530ad6d59117ad1f9e6504393f7a0e
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2024-06-25 14:28:02 +0900

    feat: [안성희] "CI 오류 해결을 위한 Jest, Babel 설정"

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    testEnvironment: 'jsdom',
+};
+  
