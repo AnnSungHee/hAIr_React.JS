a2a50462b48f85d29a2874074fa13422c6b6da27
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2024-06-25 14:37:20 +0900

    update: [안성희] "babel-preset-react-app 오류 해결을 위한 추가 설정"

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,10 @@ module.exports = {
     transform: {
       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     },
+    transformIgnorePatterns: [
+      '/node_modules/(?!axios)',
+    ],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     testEnvironment: 'jsdom',
-};
+  };
   
