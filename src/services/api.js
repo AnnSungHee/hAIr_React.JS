d73f144faf2b0fd4c9a5d0fa161d7dcd0ffc4c0d
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2024-06-26 17:44:41 +0900

    update: [안성희] "배포 환경을 위한 Axios 주소 설정"

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export const sendData = (data) => API.post('/api/endpoint', data);
