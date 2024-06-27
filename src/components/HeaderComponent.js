eaf4740e47b510420b3ee07852e0a19dd56ebe03
K19619 <157964841+K19619@users.noreply.github.com>
2024-06-27 15:10:03 +0900

    feat : [박경인] 홈페이지 추가
    home에 해당하는 페이지를 만들었습니다

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/HeaderComponentStyle.css';
+
+const HeaderComponent = () => {
+
+  return (
+    <nav>
+        <h1>헤더</h1>
+        <span className='headerContent'>
+            <div>
+                <Link to="/">
+                    <img className='companyLogo' src="/logo.png" alt="회사 로고" />
+                </Link>
+            </div>
+            <ul>
+                <li><Link to="/ai-hairstyle">AI 헤어스타일</Link></li>
+                <li><Link to="/mypage">마이페이지</Link></li>
+                <li><Link to="/logout">로그아웃</Link></li>
+            </ul>
+        </span>
+    </nav>
+  );
+};
+
+export default HeaderComponent;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../assets/styles/HomePageStyle.css';
+import HeaderComponent from '../components/HeaderComponent';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the React App</h1>
-      <Link to="/form">Go to Form</Link>
-    </div>
+    <>
+      <HeaderComponent />
+
+      <div id='mainBanner'>
+        <div><img className="bannerImg" src="/main-banner.png" alt="메인 배너 이미지" /></div>
+        <span className='links'>
+          <div><Link className='chatlink' to="/chat">채팅하기</Link></div>
+          <p>아직 계정이 없다면 <a href="">회원가입</a>하러가기</p>
+        </span>
+      </div>
+
+      <section id="serviceIntro">
+        <h1>서비스 소개</h1>
+        <section className="faceAnalyze">
+          <h1>얼굴형 분석</h1>
+          <div><img className="skelImg" src="/skel.png" alt="얼굴형 분석 사진" /></div>
+          <span>
+            <div>얼굴형 분석</div>
+            <p>무엇을 어떻게 활용한 내 얼굴형 분석</p>
+            <p>분석한 얼굴형을 토대로 맞춤스타일 추천</p>
+          </span>
+        </section>
+
+        <section className="styleRecommend">
+          <h1>맞춤 헤어스타일 추천</h1>
+          <span>
+            <div>맞춤 헤어스타일 추천</div>
+            <p>무엇을 어떻게 활용한 내 얼굴형 분석</p>
+            <p>분석한 얼굴형을 토대로 맞춤스타일 추천</p>
+          </span>
+          <div><img className="skelImg" src="/skel.png" alt="맞춤 헤어스타일 추천 사진" /></div>
+        </section>
+
+      </section>
+    </>
   );
 };
 
 export default HomePage;
+
+
+
+
+/////////////// 기존 예시코드
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to the React App</h1>
+//       <Link to="/form">Go to Form</Link>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
