3a83cd96ad656267cb7359c18ae0e0f2c6e057e8
K19619 <157964841+K19619@users.noreply.github.com>
2024-07-08 17:08:33 +0900

    feat : [박경인] 회원가입 js 구현
    spring 서버에 api요청해서 회원가입 기능 구현

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -1,46 +1,180 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import HeaderComponent from '../components/HeaderComponent';
 import '../assets/styles/pages/JoinPageStyle.css';
 
 const JoinPage = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        nickName: '',
+        city: '',
+        gender: ''
+    });
+
+    const [validationErrors, setValidationErrors] = useState({
+        email: '',
+        nickName: '',
+        gender: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleEmailBlur = async () => {
+        try {
+            const response = await axios.post(`http://localhost:8080/member/check-email/${formData.email}`);
+            if (!response.data) {
+                setValidationErrors((prevErrors) => ({
+                    ...prevErrors,
+                    email: '이 이메일은 이미 사용 중입니다.'
+                }));
+            } else {
+                setValidationErrors((prevErrors) => ({
+                    ...prevErrors,
+                    email: ''
+                }));
+            }
+        } catch (error) {
+            console.error('이메일 검증 중 오류 발생:', error);
+        }
+    };
+
+    const handleNickNameBlur = async () => {
+        try {
+            const response = await axios.post(`http://localhost:8080/member/check-nickName/${formData.nickName}`);
+            if (!response.data) {
+                setValidationErrors((prevErrors) => ({
+                    ...prevErrors,
+                    nickName: '이 닉네임은 이미 사용 중입니다.'
+                }));
+            } else {
+                setValidationErrors((prevErrors) => ({
+                    ...prevErrors,
+                    nickName: ''
+                }));
+            }
+        } catch (error) {
+            console.error('닉네임 검증 중 오류 발생:', error);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // 성별 검증 추가
+        if (!formData.gender) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                gender: '성별을 선택해주세요.'
+            }));
+            return;
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                gender: ''
+            }));
+        }
+
+        if (validationErrors.email || validationErrors.nickName || validationErrors.gender) {
+            alert('입력한 정보를 확인해주세요.');
+            return;
+        }
+
+        try {
+            const response = await axios.post('http://localhost:8080/member/sign-up', formData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (response.data) {
+                alert('회원가입이 성공적으로 완료되었습니다.');
+                window.location.replace('/');
+            } else {
+                alert('회원가입에 실패했습니다.');
+            }
+        } catch (error) {
+            console.error('회원가입 중 오류 발생:', error);
+            alert('회원가입 중 오류가 발생했습니다.');
+        }
+    };
 
     return (
         <>
-            <HeaderComponent/>
+            <HeaderComponent />
             <div className='main'>
                 <div className='joinText'>회원가입</div>
                 <div className='textInput'>
                     <label>이메일 주소*</label>
-                    <input type="text" name='email' />
+                    <input
+                        type="text"
+                        name='email'
+                        value={formData.email}
+                        onChange={handleChange}
+                        onBlur={handleEmailBlur}
+                    />
+                    {validationErrors.email && <span className='error'>{validationErrors.email}</span>}
                 </div>
                 <div className='textInput'>
                     <label>비밀번호*</label>
-                    <input type="password" name='password' />
+                    <input
+                        type="password"
+                        name='password'
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className='textInput'>
                     <label>닉네임*</label>
-                    <input type="text" name='username' />
+                    <input
+                        type="text"
+                        name='nickName'
+                        value={formData.nickName}
+                        onChange={handleChange}
+                        onBlur={handleNickNameBlur}
+                    />
+                    {validationErrors.nickName && <span className='error'>{validationErrors.nickName}</span>}
                 </div>
                 <div className='textInput'>
                     <label>주소</label>
-                    <input type="text" name='location' />
+                    <input
+                        type="text"
+                        name='city'
+                        value={formData.city}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className='radioInput'>
                     <label>성별*</label>
                     <div>
                         <div>
                             <label>남성</label>
-                            <input type="radio" name='gender' value="male" />
+                            <input
+                                type="radio"
+                                name='gender'
+                                value="male"
+                                checked={formData.gender === 'male'}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div>
                             <label>여성</label>
-                            <input type="radio" name='gender' value="female" />
+                            <input
+                                type="radio"
+                                name='gender'
+                                value="female"
+                                checked={formData.gender === 'female'}
+                                onChange={handleChange}
+                            />
                         </div>
                     </div>
+                    {validationErrors.gender && <span className='error'>{validationErrors.gender}</span>}
                 </div>
-                
-                <div className='submitBtn'>가입하기</div>
+                <div className='submitBtn' onClick={handleSubmit}>가입하기</div>
             </div>
         </>
     );
