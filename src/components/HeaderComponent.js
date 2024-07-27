fb9c5fe0e7ba0d7a5d57be002969f912ded0634f
DESKTOP-0L11UB0\user <ssongjiaran@gmail.com>
2024-07-27 14:03:22 +0900

    add : [송가람] 반응형 웹으로 수정

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import '../assets/styles/components/HeaderComponentStyle.css';
 import axios from 'axios';
 
 const HeaderComponent = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
     const token = localStorage.getItem('token');
@@ -24,21 +25,20 @@ const HeaderComponent = () => {
         navigate('/');  // 로그아웃 후 홈 페이지로 리디렉션 
     };
 
-    // Debugging: 현재 경로를 출력해봅시다.
-    console.log('Current location:', location.pathname);
-
     const isActive = (path) => location.pathname === path || location.pathname.startsWith(path);
 
     return (
         <nav id='header'>
-            <h1>헤더</h1>
-            <span className='headerContent'>
-                <div>
-                    <Link to="/">
-                        <img className='logo' src="/logo.png" alt="회사 로고" />
-                    </Link>
+            <div className='headerContent'>
+                <Link to="/">
+                    <img className='logo' src="/logo.png" alt="회사 로고" />
+                </Link>
+                <div className={`menuToggle ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
                 </div>
-                <ul className='linkList'>
+                <ul className={`linkList ${menuOpen ? 'open' : ''}`}>
                     <li>
                         <Link 
                             to="/chatpage" 
@@ -82,9 +82,102 @@ const HeaderComponent = () => {
                         </>
                     )}
                 </ul>
-            </span>
+            </div>
         </nav>
     );
 };
 
 export default HeaderComponent;
+
+
+
+// import React from 'react';
+// import { Link, useNavigate, useLocation } from 'react-router-dom';
+// import '../assets/styles/components/HeaderComponentStyle.css';
+// import axios from 'axios';
+
+// const HeaderComponent = () => {
+//     const navigate = useNavigate();
+//     const location = useLocation();
+//     const token = localStorage.getItem('token');
+
+//     const handleLogout = async () => {
+//         await axios.post('http://localhost:8080/member/logout', {}, {
+//             headers: {
+//                 'Authorization': `Bearer ${token}`,
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+
+//         localStorage.removeItem('token');
+//         localStorage.removeItem('id');
+//         localStorage.removeItem('nickName');
+
+//         alert('로그아웃이 성공적으로 완료되었습니다.');
+//         navigate('/');  // 로그아웃 후 홈 페이지로 리디렉션 
+//     };
+
+//     // Debugging: 현재 경로를 출력해봅시다.
+//     console.log('Current location:', location.pathname);
+
+//     const isActive = (path) => location.pathname === path || location.pathname.startsWith(path);
+
+//     return (
+//         <nav id='header'>
+//             <h1>헤더</h1>
+//             <span className='headerContent'>
+//                 <div>
+//                     <Link to="/">
+//                         <img className='logo' src="/logo.png" alt="회사 로고" />
+//                     </Link>
+//                 </div>
+//                 <ul className='linkList'>
+//                     <li>
+//                         <Link 
+//                             to="/chatpage" 
+//                             className={isActive('/chatpage') ? 'selectedLink' : ''}
+//                         >
+//                             ChatBot
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link 
+//                             to="/ai-hairstyle" 
+//                             className={isActive('/ai-hairstyle') ? 'selectedLink' : ''}
+//                         >
+//                             AI Simulation
+//                         </Link>
+//                     </li>
+//                     <li>
+//                         <Link 
+//                             to="/mypage" 
+//                             className={isActive('/mypage') ? 'selectedLink' : ''}
+//                         >
+//                             Mypage
+//                         </Link>
+//                     </li>
+//                     {token ? (
+//                         <li>
+//                             <div onClick={handleLogout} style={{ cursor: 'pointer' }}>
+//                                 Log out
+//                             </div>
+//                         </li>
+//                     ) : (
+//                         <>
+//                             <li>
+//                                 <Link 
+//                                     to="/sign-in" 
+//                                     className={isActive('/sign-in') || isActive('/join') ? 'selectedLink' : ''}
+//                                 >
+//                                     Log in
+//                                 </Link>
+//                             </li>
+//                         </>
+//                     )}
+//                 </ul>
+//             </span>
+//         </nav>
+//     );
+// };
+
+// export default HeaderComponent;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,146 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
 import { Link } from 'react-router-dom';
 import '../assets/styles/pages/HomePageStyle.css';
 import HeaderComponent from '../components/HeaderComponent';
 
 const HomePage = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const totalSlides = 3;
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % totalSlides);
-    }, 2600); // 1.5초마다 슬라이드 이동
-
-    return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 클리어
-  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
 
   return (
     <>
       <HeaderComponent />
       <section id="main">
-        <div className="pageNcontents" style={{ transform: `translateX(-${currentSlide * 110}%)` }}>
-          <span className="pageNcontent">
-            <span className="textNImg">
-              <span className="textBox">
-                <p className="engScript engScript1">Personalized <span>Style Services</span></p>
-                <p className="korScript korScript1">나만의 AI 헤어 상담사와의 대화로 나만의 스타일을 완성하세요</p>
+        <Slider {...settings}>
+          <div className="pageNcontents">
+            <div className="textNImg">
+              <div className="textBox">
+                <p className="engScript engScript1">Personalized<br></br><span>Style Services</span></p>
+                <p className="korScript">나만의 AI 헤어 상담사와의 대화로 나만의 스타일을 완성하세요</p>
                 <div className="tryChatBtn"><Link to="/chatpage">체 험 하 기</Link></div>
-              </span>
-              <div><img className="bannerImg1" src="/women1.png" alt="여자1" /></div>
-            </span>
+              </div>
+              <div><img className="bannerImg" src="/women1.png" alt="여자1" /></div>
+            </div>
             <div className="pageIndicator">01/<span>03</span></div>
-          </span>
-          <span className="pageNcontent">
-            <span className="textNImg">
-              <span className="textBox">
-                <p className="engScript engScript2">AI-based <span>Face Analysis</span></p>
-                <p className="korScript korScript2">분석한 내 얼굴형을 토대로 내 맞춤 스타일을 추천받으세요</p>
-              </span>
-              <div><img className="bannerImg2" src="/man1.png" alt="남자1" /></div>
-            </span>
+          </div>
+          <div className="pageNcontents">
+            <div className="textNImg">
+              <div className="textBox">
+                <p className="engScript engScript2">AI-based<br></br><span>Face Analysis</span></p>
+                <p className="korScript">분석한 내 얼굴형을 토대로 내 맞춤 스타일을 추천받으세요</p>
+                <div className="tryChatBtn"></div>
+              </div>
+              <div><img className="bannerImg" src="/man1-1.png" alt="남자1" /></div>
+            </div>
             <div className="pageIndicator">02/<span>03</span></div>
-          </span>
-          <span className="pageNcontent">
-            <span className="textNImg">
-              <span className="textBox">
-                <p className="engScript engScript3">AI-hairstyle <span>Simulation</span></p>
-                <p className="korScript korScript3">대화를 통해 추천받은 헤어스타일을 사용자의 얼굴에 시뮬레이션 해보세요</p>
-              </span>
-              <div><img className="bannerImg3" src="/women2.png" alt="여자2" /></div>
-            </span>
-            <div className="pageIndicator pageIndicator3">03/<span>03</span></div>
-          </span>
-        </div>
+          </div>
+          <div className="pageNcontents">
+            <div className="textNImg">
+              <div className="textBox">
+                <p className="engScript">AI-hairstyle<br></br><span>Simulation</span></p>
+                <p className="korScript">대화를 통해 추천받은 헤어스타일을 사용자의 얼굴에 시뮬레이션 해보세요</p>
+                <div className="tryChatBtn"></div>
+              </div>
+              <div><img className="bannerImg" src="/women2-1.png" alt="여자2" /></div>
+            </div>
+            <div className="pageIndicator">03/<span>03</span></div>
+          </div>
+        </Slider>
       </section>
     </>
   );
 };
 
 export default HomePage;
-
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import '../assets/styles/pages/HomePageStyle.css';
-// import HeaderComponent from '../components/HeaderComponent';
-
-// const HomePage = () => {
-
-//   return (
-//     <>
-//       <HeaderComponent />
-
-//       <section id="main">
-
-//         <div className='pageNcontents'>
-//           <span className='pageNcontent'>
-//             <span className='textNImg'>
-//               <span className='textBox'>
-//                 <p className='engScript engScript1'>Personalized<span>Style Services</span></p>
-//                 <p className='korScript korScript1'>나만을 위한 AI 헤어 상담사와의 대화로 나만의스타일을 완성하세요</p>
-//                 <div className='tryChatBtn'><Link to="/chatpage">체 험 하 기</Link></div>
-//               </span>
-//               <div><img className='bannerImg1' src="/women1.png" alt="여자1" /></div>
-//             </span>
-//             <div className='pageIndicator'>01/<span>03</span></div>
-//           </span>
-
-//           <span className='pageNcontent'>
-//             <span className='textNImg'>
-//               <span className='textBox'>
-//                 <p className='engScript engScript2'>AI-based <span>Face Analysis</span></p>
-//                 <p className='korScript korScript2'>분석한 내 얼굴형을 토대로 내 맞춤 스타일을 추천받으세요</p>
-//                 {/* <div className='tryChatBtn'><Link to="/chatpage">체 험 하 기</Link></div> */}
-//               </span>
-//               <div><img className='bannerImg2' src="/man1.png" alt="남자1" /></div>
-//             </span>
-//             <div className='pageIndicator'>02/<span>03</span></div>
-//           </span>
-
-//           <span className='pageNcontent'>
-//             <span className='textNImg'>
-//               <span className='textBox'>
-//                 <p className='engScript engScript3'>AI-hairstyle<span>Simulation</span></p>
-//                 <p className='korScript korScript3'>대화를 통해 추천받은 헤어스타일을 사용자의 얼굴에 시뮬레이션 해보세요</p>
-//                 {/* <div className='tryChatBtn'><Link to="/chatpage">체 험 하 기</Link></div> */}
-//               </span>
-//               <div><img className='bannerImg3' src="/women2.png" alt="여자2" /></div>
-//             </span>
-//             <div className='pageIndicator pageIndicator3'>03/<span>03</span></div>
-//           </span>
-
-//         </div>
-//         {/* <div className='iconList'>
-//           <img src="/twitter.png" alt="트위터" />
-//           <img src="/instagram.png" alt="인스타그램" />
-//           <img src="/facebook.png" alt="페이스북" />
-//           <img src="/v.png" alt="브이" />
-//         </div> */}
-//       </section>
-//     </>
-//   );
-// };
-
-// export default HomePage;
-
-
-
-
-/////////////// 기존 예시코드
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const HomePage = () => {
-//   return (
-//     <div>
-//       <h1>Welcome to the React App</h1>
-//       <Link to="/form">Go to Form</Link>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
